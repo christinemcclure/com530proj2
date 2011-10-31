1f6e0776b2eddb8a5a28b11ffbf29bea622c6f59
Christine McClure <camcclure@gmail.com>
2011-10-31 15:48:41 -0500

    created html head stub. put bio back into main page

--- portraits.php
+++ portraits.php
@@ -1,49 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" id="christinemcclure-com" class="t22 g978">
-
-<head>
-
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <!--
-    Use device width on iPhone, no scaling; maximum-scale keeps things cool on orientation changes.
-  -->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-
+<?php include '/includes/html-head-stub.html'; ?>
   <title>Portraiture Gallery: Christine McClure</title>
-  <link href='http://fonts.googleapis.com/css?family=Quattrocento+Sans' rel='stylesheet' type='text/css' /> <!-- paragraph Google font -->
-  <link href='http://fonts.googleapis.com/css?family=Lato:700,900' rel='stylesheet' type='text/css' /> <!-- headings Google font -->
-  <link rel="stylesheet" type="text/css" href="/css/reset.css" />
-  <link rel="stylesheet" type="text/css" media="screen" href="/css/grid.css" /> 
-  <link rel="stylesheet" type="text/css" media="screen" href="/css/screen.css" />
-  <!--[if lte IE 7>
-  <link rel="stylesheet" type="text/css" media="screen" href="/css/screen-ie.css" />
-  <![endif]-->
-  <link rel="stylesheet" type="text/css" media="print" href="/css/print.css" />
-
-<!--REMOVE COMMENT TAGS TO USE FACEBOOK SHARE METADATA:-->
-
-<!--
-  <meta name="title" content="Match this with the content of the title tag above." /> 
-  <meta name="description" content="A short description of this page." />
-  <link rel="image_src" href="http://example.com/media/img/thumbnail.jpg" />
--->
-
-<!--REMOVE COMMENT TAGS TO USE FAVICON LINKS:-->
-
-  <!--For most browsers:-->
-<!--
-  <link rel="icon" href="http://example.com/favicon.png" type="image/png" />
--->
-  <!--For Internet Explorer:-->
-<!--
-  <link rel="shortcut icon" href="http://example.com/favicon.ico" />
--->
-
-  <!--Load the latest version of 1. jQuery library-->
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-  <!--Load site-wide JavaScript-->
-  <script type="text/javascript" src="/js/site.js"></script>
-
 </head>
 
 <body>
