31cc32a3cd9be092fa757eef03ee73cb4a773bb6
Christine McClure <camcclure@gmail.com>
2011-11-03 14:26:21 -0500

    spacing for mobile headings

--- index.php
+++ index.php
@@ -16,6 +16,30 @@
 			<h2>Photography</h2>
       <p>I've been fascinated with photography ever since my childhood in the seventies, where I staged a photo shoot using a velour bathrobe as a backdrop, ceramic dog figurines as models, and my trusty little 110 plastic camera.</p>
           
+          <h3 id="openGalleries">
+            <span class="headingblock">Galleries:</span> <span class="subheading headingblock">View past work</span>
+          </h3>
+          <div id="galleries" class="clear"> 
+          	<h4><a href="/fine-art.php">Fine Art:<span class="subheading">Images from the Portals exhibition.</span></a></h4>  
+          	<h4><a href="/portraits.php">Portraiture:<span class="subheading">Headshots and other stagings.</span></a></h4>
+          	<h4><a href="/events.php">Events:<span class="subheading">Candid shots from group events.</span></a></h4>
+						<h4> <a href="/travel.php">Travel:<span class="subheading">Favorite images from my travels.</span></a></h4>  
+        
+          </div>
+
+          <div id="openPricing">
+            <h3><span class="headingblock">Pricing:</span> <span class="subheading headingblock">How things are done</span></h3>
+          </div>
+          <div id="pricing" class="clear"> 
+          <h4>Event Photography</h4>
+            <p>From children's birthday parties to book signings to corporate functions, you'll receive an accurate record of the occasion. Pricing is by a per-hour shooting fee, with a minimum agreed-upon amount. Additional shooting time can be arranged on the day of the event if necessary.</p>
+             <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 150ppi). Print-quality images can be ordered onlineat an additional cost.</p>
+          <h4>Portraiture</h4>
+            <p>Whether you want a professional portrait, a casual outdoor photo, or something inbetween, I can work with you to capture the image you want to present to the world. With travel lights and a tripod, I can transfer the image in your mind to reality.</p>
+             <p>Pricing is based on a flat-rate sitting fee for one hour of shooting. Additional time can be purchased for location and wardrobe changes.</p>
+             <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 150ppi). Print-quality images can be ordered online at an additional cost.</p>
+          </div>
+
           <h3 id="openAbout">
 						<span class="headingblock">About:</span> <span class="subheading headingblock">Exhibitions/Education</span>
           </h3>
@@ -39,29 +63,6 @@
               	<p>Basic exposure and printing, advanced printing, color printing, digital imaging, portraiture lighting. 1994 - present</p>
 					</div>
           
-          <h3 id="openGalleries">
-            <span class="headingblock">Galleries:</span> <span class="subheading headingblock">View past work</span>
-          </h3>
-          <div id="galleries" class="clear"> 
-          	<h4><a href="/fine-art.php">Fine Art</a></h4>  
-          	<h4><a href="/portraits.php">Portraiture</a></h4>
-          	<h4><a href="/events.php">Events</a></h4>
-						<h4> <a href="/travel.php">Travel</a></h4>  
-        
-          </div>
-
-          <div id="openPricing">
-            <h3><span class="headingblock">Pricing:</span> <span class="subheading headingblock">How things are done</span></h3>
-          </div>
-          <div id="pricing" class="clear"> 
-          <h4>Event Photography</h4>
-            <p>From children's birthday parties to book signings to corporate functions, you'll receive an accurate record of the occasion. Pricing is by a per-hour shooting fee, with a minimum agreed-upon amount. Additional shooting time can be arranged on the day of the event if necessary.</p>
-             <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 150ppi). Print-quality images can be ordered onlineat an additional cost.</p>
-          <h4>Portraiture</h4>
-            <p>Whether you want a professional portrait, a casual outdoor photo, or something inbetween, I can work with you to capture the image you want to present to the world. With travel lights and a tripod, I can transfer the image in your mind to reality.</p>
-             <p>Pricing is based on a flat-rate sitting fee for one hour of shooting. Additional time can be purchased for location and wardrobe changes.</p>
-             <p>After the event, you will receive a CD of web-quality images (600x400 pixels at 150ppi). Print-quality images can be ordered online at an additional cost.</p>
-          </div>
 
           <div id="openContact">
             <h3><span class="headingblock">Contact:</span> <span class="subheading headingblock">Book or order prints</span></h3>
