2ecee513a0a00033eab1980a0a3ee9f11c4a96a3
Christine McClure <camcclure@gmail.com>
2011-11-12 11:02:59 -0600

    removed lab include and made its own page

---
+++ lab.php
@@ -0,0 +1,27 @@
+<?php include './includes/html-head-stub.html'; ?>
+  <title>Lab: Christine McClure</title>
+</head>
+
+<body>
+
+<div id="theLab" class="page">
+	<?php include './includes/header.html'; ?>
+
+ 	<?php include './includes/lib-nav.html'; ?>
+ 
+  <div id="content" class="clear">
+
+<h2>Ruby on Rails</h2>
+<p>I've been learning a bit about the Ruby programming language using the Rails framework. It's slow-going because it's wuch a terse language, but you can see a small example of an application I'm currently developing at <a href="http://electric-ice-8535.heroku.com/">the Heroku site.</a></p>
+
+
+  </div>
+ 
+  <?php include 'includes/footer.html' ?> 
+
+
+</div>
+
+</body>
+
+</html>
