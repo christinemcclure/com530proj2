7259de775d3ef87e1ec0a187b27321eed1d34eab
Christine McClure <camcclure@gmail.com>
2011-11-13 17:50:26 -0600

    added portraits

--- portraits.php
+++ portraits.php
@@ -13,10 +13,14 @@
   <div id="content" class="clear">
 
     <ul>
-      <li><p>Gerry Doyle, <strong>From the Depths</strong> book jacket portrait. </p><img src="/img/portraits/gd_book_03.jpg" alt="Photo of author Gerry Doyle in a bar, holding a beer and laughing." /></li>
+      <li>
+        <p>Nadia's first photo shoot.</p><img src="/img/portraits/baby.jpg" alt="Photo of an 8-month old baby in a garden." /></li>
+      <li>
+        <p>Family portrait day.</p><img src="/img/portraits/family.jpg" alt="Photo of a a multi-racial family in a garden." /></li>
       <li><p>Gerry Doyle, <strong>From the Depths</strong> book jacket shoot.</p> <img src="/img/portraits/gd_book_02.jpg" alt="Black and white photo of author Gerry Doyle holding a beer, with the back of a woman's head in the foreground." /></li>
+      <li><p>Ariana's second birthday. </p><img src="/img/portraits/birthday.jpg" alt="photo of a stwo-year-old girl eating a cookie with powdered sugar on her face." /></li>
       <li><p>Singer, Literature for all of Us benefit at Cary's Lounge, Chicago IL. </p><img src="/img/portraits/2009ThisisHell-LFAOU115.jpg" alt="Black and white photo of a smiling singer, head turned to the side." /></li>
-			<li><p>Participant, Greek Independence Day Parade, Chicago IL. </p><img src="/img/portraits/Lety_Greek_Parade-337.jpg" alt="Young girl in colorful traditional Greek headscarf." /></li>
+				<li><p>Participant, Greek Independence Day Parade, Chicago IL. </p><img src="/img/portraits/Lety_Greek_Parade-337.jpg" alt="Young girl in colorful traditional Greek headscarf." /></li>
     </ul>
   
   </div>
