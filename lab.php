2ac3439603cd6c0c59480759a94e77853ac86898
Christine McClure <camcclure@gmail.com>
2011-11-14 16:51:17 -0600

    all layout done except for home page. tweak resume if time

--- lab.php
+++ lab.php
@@ -8,10 +8,11 @@
 	<?php include './includes/header.html'; ?>
 
  	<?php include './includes/lib-nav.html'; ?>
+ 	<?php include './includes/nav.html'; ?>
  
   <div id="content" class="clear">
 
-    <h2>Ruby on Rails</h2>
+    <h3>Ruby on Rails</h3>
     <p>I've been learning a bit about the Ruby programming language using the Rails framework. It's slow-going because it's such a terse language, but you can see a small example of an application I'm currently developing at <a href="http://electric-ice-8535.heroku.com/">the Heroku site.</a></p>
     
 
