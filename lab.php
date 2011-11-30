95f80417ea62df873f3f7a6bc4c2f0bb652bc004
Christine McClure <camcclure@gmail.com>
2011-11-29 19:22:46 -0600

    added new home page link to lab

--- lab.php
+++ lab.php
@@ -12,6 +12,11 @@
  
   <div id="content" class="clear">
 
+    <h3>New Home Page</h3>
+    <p>Once I put my new site live, I realized that I hated the layout (of course). I had spent so much time 
+    thinking about responsive-ness and mobile-first that I hadn't paid enough attention to the design. 
+    So here is the <a href="http://dev.christinemcclure.com">new home page</a> I'm envisioning.</p>
+
     <h3>Ruby on Rails</h3>
     <p>I've been learning a bit about the Ruby programming language using the Rails framework. It's slow-going because it's such a terse language, but you can see a small example of an application I'm currently developing at <a href="http://electric-ice-8535.heroku.com/">the Heroku site.</a></p>
     
